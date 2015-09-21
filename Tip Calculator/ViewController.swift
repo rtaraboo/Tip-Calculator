314806da1ef76a59e00218df999ae1f2f3b6fc69
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-21 16:30:22 -0400

    Initial Commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtMealCost: UITextField!
+    @IBOutlet weak var txtTipPercentage: UITextField!
+    
+    @IBOutlet weak var lblTip: UILabel!
+    @IBOutlet weak var lblTipTotal: UILabel!
+    
+    var mealCost = ""
+    var tipPercentage = ""
+    var totalTip : Float = 0.0
+    var totalMealCost : Float = 0.0
+    
+    
+    
+    @IBAction func btnCalcutaleACTION(sender: UIButton) {
+        
+        calcutateTip()
+    }
+    
+    
+    
+    @IBAction func btnClearACTION(sender: UIButton) {
+        
+        clear()
+    
+    
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +49,69 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    func calcutateTip(){
+        
+        mealCost = txtMealCost.text!
+        tipPercentage = txtTipPercentage.text!
+        
+        let fMealCost = Float(mealCost)
+        let fTipPercentage = Float(tipPercentage)
+        
+        totalTip = fMealCost! * (fTipPercentage! / 100)
+        totalMealCost = fMealCost! + totalTip
+        
+        printTip()
+        
+        }
+    
+    
+    
+    func printTip(){
+        let formatTip = String(format: "%0.2f", totalTip)
+        let formatTotal = String(format: "%0.2f", totalMealCost)
+        
+        lblTip.text = "Tip: $\(formatTip)"
+        lblTipTotal.text = "$\(formatTotal)"
+        
+        hideKeyboards()
+        
+        
+    }
+    
+    
+    func clear(){
+        
+        txtMealCost.text = ""
+        txtTipPercentage.text = ""
+        
+        lblTip.text = "Tip: $0.00"
+        lblTipTotal.text = ""
+        
+        hideKeyboards()
+        
+        }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        hideKeyboards()
+    
+    }
+    
+    
+    func hideKeyboards(){
+        txtMealCost.resignFirstResponder()
+        txtTipPercentage.resignFirstResponder()
+        
+    }
+    
+    
+    
+    
+    
+    
 
 
 }
